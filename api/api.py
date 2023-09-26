942849f9ef5f57d49dc1e258cde9b5ea8ced837e
Baris Salcan <barissalcan@web.de>
2023-09-26 10:37:17 +0200

    Test & clean code

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 import flask
-from flask import request   # wird benötigt, um die HTTP-Parameter abzufragen
-from flask import jsonify   # übersetzt python-dicts in json
+from flask import request  # wird benötigt, um die HTTP-Parameter abzufragen
+from flask import jsonify  # übersetzt python-dicts in json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True  # Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
 
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Tischreservierung</h1>"
+
 
 app.run()
