a935b233391a4c7994b2ea5c403ef5042756c7a7
Jordan Abi <jordan.zeng@hotmail.de>
2023-10-17 10:01:12 +0200

    timeslot

--- api/api.py
+++ api/api.py
@@ -32,5 +32,30 @@ def api_all():
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
+@app.route('/api/v1/resources/timeslot', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    zeitpunkt = query_parameters.get('zeitpunkt')
+    query = "SELECT zeitpunkt FROM reservierungen WHERE"
+    to_filter = []
+
+    if zeitpunkt:
+        query += ' zeitpunkt=? '
+        to_filter.append(zeitpunkt)
+
+    if not (zeitpunkt):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('../buchungssystem.sqlite')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
 
 app.run()
